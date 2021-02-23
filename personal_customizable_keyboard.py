6424eb08c84b4e10fb0733c2a6de9e2899b544ed
shmlktz <76396773+shmlktz@users.noreply.github.com>
2021-02-23 04:22:19 +0200

    Add files via upload
    -preserved the sunken raise pattern code as a comment at the bottom
    -packed in two rows
    -added Trello desktop link functionality
    --for a Trello board in general, and for a specific Trello card
    
    (should really go back to how I got the staircase patterns and 4 way button patterns going)

--- personal_customizable_keyboard.py
+++ personal_customizable_keyboard.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 ##lets make a personal customizable keyboard
 window = tk.Tk()
@@ -6,27 +7,36 @@ window = tk.Tk()
 ##open links
 import webbrowser
 def runLink():
-    this = webbrowser.open('evernote:///view/21614375/s192/10b4247b-5f09-4b66-953d-02e5f27aac3c/10b4247b-5f09-4b66-953d-02e5f27aac3c/')
+    choose = random.randint(1,4)
+    print(choose)
+    if choose == 1:
+        this = webbrowser.open('evernote:///view/21614375/s192/10b4247b-5f09-4b66-953d-02e5f27aac3c/10b4247b-5f09-4b66-953d-02e5f27aac3c/')
+    elif choose == 2:
+        that = webbrowser.open('trello://trello.com/b/kGXNJK3x/עכשיו')
+    elif choose == 3:
+        webbrowser.open('https://trello.com/c/Ljfh6gZq')
+    elif choose == 4:
+        webbrowser.open('trello://trello.com/c/Ljfh6gZq')
 #this = "evernote:///view/21614375/s192/10b4247b-5f09-4b66-953d-02e5f27aac3c/10b4247b-5f09-4b66-953d-02e5f27aac3c/"
-for i in range(8):
-    frame = tk.Frame(master = window, relief = "sunken", borderwidth = 10)
-    frame.pack(side = "left")
-    b = tk.Button(text ="[A Key]", master = frame, width = 16, height = 5, command = runLink) #command = helloCallBack)
-    b.pack()
-    frame_2 = tk.Frame(master = window, relief = "raised", borderwidth = 10)
-    frame_2.pack(side = tk.LEFT)
-    b = tk.Button(text ="[B Key]", master = frame_2, width = 16, height = 5, command = runLink) #command = helloCallBack)
-    b.pack()
-    # frame = tk.Frame(master = window, relief = "raised", borderwidth = 10)
-    # frame.pack(side = tk.RIGHT)
-    # b = tk.Button(text ="[A Key]", master = frame, width = 16, height = 5, command = runLink) #command = helloCallBack)
-    # b.pack()
+topFrame = tk.Frame(master = window, relief = "sunken", borderwidth = 10)
+topFrame.pack()
+bottomFrame = tk.Frame(master = window, relief = "raised", borderwidth = 10)
+bottomFrame.pack()
+for i in range(2):
+    b = tk.Button(text ="[A Key]", master = topFrame, width = 16, height = 5, command = runLink) #command = helloCallBack)
+    b.pack(side = "left")
+    #
+    b = tk.Button(text ="[B Key]", master = bottomFrame, width = 16, height = 5, command = runLink) #command = helloCallBack)
+    b.pack(side = "left")
+    #
+    b = tk.Button(text ="[A Key]", master = topFrame, width = 16, height = 5, command = runLink) #command = helloCallBack)
+    b.pack(side = "left")
+    #
+    b = tk.Button(text ="[B Key]", master = bottomFrame, width = 16, height = 5, command = runLink) #command = helloCallBack)
+    b.pack(side = "left")
 
-    # frame = tk.Frame(master = window, relief = "raised", borderwidth = 10)
-    # frame.pack(side = tk.TOP)
-    # b = tk.Button(text ="[A Key]", master = frame, width = 16, height = 5, command = runLink) #command = helloCallBack)
-    # b.pack()
 
+####Evernote workspace
 #Building a button from the components of a [Right Click and "Copy Note Link"] evernote link
 #STRUCTURES###
 #desktop link: evernote:///view/[userId]/[shardId]/[noteGuid]/[noteGuid]/
@@ -45,8 +55,13 @@ webbrowser.open(new_desktop_evernote_link)
 window.mainloop()
 
 
-
-# name_entry = tk.Entry(
-#     fg="white", bg="black", width = 16, master = frame ) #fg = text; bg = background
-# name_entry.pack()
-# name_entry.insert(0, "_name_")
+##beautiful sunken/raised frame packing pattern
+# for i in range(8):
+#     frame = tk.Frame(master = window, relief = "sunken", borderwidth = 10)
+#     frame.pack(side = "left")
+#     b = tk.Button(text ="[A Key]", master = frame, width = 16, height = 5, command = runLink) #command = helloCallBack)
+#     b.pack()
+#     frame_2 = tk.Frame(master = window, relief = "raised", borderwidth = 10)
+#     frame_2.pack(side = tk.LEFT)
+#     b = tk.Button(text ="[B Key]", master = frame_2, width = 16, height = 5, command = runLink) #command = helloCallBack)
+#     b.pack()
